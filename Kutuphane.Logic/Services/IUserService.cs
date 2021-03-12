ab306744e71bf5af756dbc31b551d3b057431b5c
Ceyhun Boran <cey.boran@hotmail.com>
2021-03-12 03:57:20 +0300

    Authentication Service, Token Generation

---
+++ Kutuphane.Logic/Services/IUserService.cs
@@ -0,0 +1,76 @@
+﻿using Kutuphane.Data.Entities;
+using Kutuphane.Logic.Helpers;
+using Kutuphane.Data.Entities.DTO.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.Logic.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<Member> GetAll();
+        Member GetById(int id);
+    }
+    public class UserService : IUserService
+    {
+        private List<Member> _users = new List<Member>
+        {
+            new Member { Id = 1, Name = "Test", Age = 19,Adress = "Adress", Username = "test", Password = "password"}
+        };
+
+        private readonly AppSettings _appSettings;
+
+        public UserService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest model)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+        }
+
+        public IEnumerable<Member> GetAll()
+        {
+            return _users;
+        }
+
+        public Member GetById(int id)
+        {
+            return _users.FirstOrDefault(x => x.Id == id);
+        }
+
+        // helper methods
+
+        private string generateJwtToken(Member user)
+        {
+            //Generating token that is available for 60 minutes
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
